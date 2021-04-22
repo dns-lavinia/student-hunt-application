8e80fc1e28686b49de81d8b3368c6089248eba06
Lavinia Dinis <andreea.dinis@student.upt.ro>
2021-04-22 19:28:15 +0300

    Added the method for hashing a password and for checking a password

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
         SingInFrame frame = new SingInFrame();
 
+        PasswordAuthentication pass = new PasswordAuthentication();
 
+        //----- this part should be deleted afterwards, it s just temporary for testing purposes
+        String hs = pass.hashPassword("12345");
+        boolean b = pass.checkPassword("12345", hs);
+
+        if(b == true)
+            System.out.println("Correct password");
+        else
+            System.out.println("incorrect pass");
     }
 }
--- src/main/java/PasswordAuthentication.java
+++ src/main/java/PasswordAuthentication.java
@@ -1,16 +1,76 @@
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
 public class PasswordAuthentication {
-    private static final int DEFAULT_COST = 16;
     private final int cost;
 
     public PasswordAuthentication() {
-        this(DEFAULT_COST);
+       cost = 16;
     }
 
-    public PasswordAuthentication(int cost) {
-        // iterations(cost)
-        this.cost = cost;
+    /**
+     * This method uses the PBKDF2 algorithm for hashing a user's password
+     * @param pass_plaintext The plaintext of the password introduced by the user
+     * @return On success, a byte array is returned with the hashed password, otherwise null is returned
+     */
+    public String hashPassword(String pass_plaintext) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[cost];
+        random.nextBytes(salt);
+
+        KeySpec spec = new PBEKeySpec(pass_plaintext.toCharArray(), salt, 1000, 128);
+
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+
+            Base64.Encoder enc_salt = Base64.getUrlEncoder().withoutPadding();
+            Base64.Encoder enc_hash = Base64.getUrlEncoder().withoutPadding();
+            return 1000 + ":" + enc_salt.encodeToString(salt) + ":" + enc_hash.encodeToString(hash);
+
+        } catch(NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+        // if it gets till here, something went wrong
+        return null;
     }
 
-    // I'm thinking to use this class both for login and for registration, to generate a hash
-    // or to check the introduced password
+
+    public boolean checkPassword(String pass_plaintext, String hashedPassword) {
+        if(hashedPassword == null)
+            throw new java.lang.IllegalArgumentException("Invalid hash provided for comparison");
+
+        String[] parts = hashedPassword.split(":");
+        int iterations = Integer.parseInt(parts[0]);
+        Base64.Decoder dec = Base64.getUrlDecoder();
+        byte[] salt = dec.decode(parts[1]);
+        byte[] hash = dec.decode(parts[2]);
+
+
+        PBEKeySpec spec = new PBEKeySpec(pass_plaintext.toCharArray(), salt, iterations, hash.length * 8);
+
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] testHash = skf.generateSecret(spec).getEncoded();
+
+            int diff = hash.length ^ testHash.length;
+            for(int i = 0; i < hash.length && i < testHash.length; i++)
+            {
+                diff |= hash[i] ^ testHash[i];
+            }
+            return diff == 0;
+
+        } catch(NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
